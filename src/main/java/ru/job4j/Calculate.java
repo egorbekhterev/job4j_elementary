7ccb0603658bcb1c011be5b6cd10153b7af78e4d
Paranoj <e.behterev@yandex.ru>
2022-03-12 13:34:36 +0700

    0. Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Calculate {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0032F;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+    }
+}
