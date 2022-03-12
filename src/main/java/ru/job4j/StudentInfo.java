e882ab7fc39eaccfc1b8b05ce212856d8836d322
Paranoj <e.behterev@yandex.ru>
2022-03-12 11:59:17 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Egor Bekhterev");
+        System.out.println("27.09.1997");
+    }
+}
