314676e275ca3478fff69c934c2023c46efa5586
Paranoj <e.behterev@yandex.ru>
2022-04-20 19:36:17 +0700

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -5,8 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        names[0] = "Egor";
+        names[1] = "Ivan";
+        names[2] = "Maria";
+        names[3] = "Julia";
+        System.out.println("Список имён: " + names[0] + " " + names[1] + " " + names[2] + " " + names[3]);
     }
 }
