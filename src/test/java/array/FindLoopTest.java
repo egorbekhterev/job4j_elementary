c50e3625e896dbe1f70ec3a3e0574580c1d1dba4
Paranoj <e.behterev@yandex.ru>
2022-04-25 02:01:28 +0700

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/array/FindLoopTest.java
+++ src/test/java/array/FindLoopTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas5Then0() {
+    public void whenArrayHas5Then00() {
         int[] data = {5, 4, 3, 2};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
@@ -21,6 +21,37 @@ public class FindLoopTest {
         int[] data = {1, 2, 3, 4, 8};
         int el = 10;
         int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = new int[]{5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[]{1, 2, 3, 7, 9};
+        int el = 8;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
