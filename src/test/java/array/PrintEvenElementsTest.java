b00da0eef3381e620fb2648fb632a361ffe13f58
Paranoj <e.behterev@yandex.ru>
2022-04-28 20:15:43 +0700

    6.4.1.1. Обход массива с последнего элемента [#361619 #293127]

--- src/test/java/array/PrintEvenElementsTest.java
+++ src/test/java/array/PrintEvenElementsTest.java
@@ -15,4 +15,12 @@ public class PrintEvenElementsTest {
         assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
     }
 
+    @Test
+    public void whenArrayHasOddNumberOfElements() {
+        String ln = System.lineSeparator();
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        String expected = "Элементы массива с чётными индексами, начиная с последнего: " + ln
+                + 15 + ln + 13 + ln + 11 + ln + 9 + ln + 7 + ln + 5 + ln + 3 + ln + 1 + ln;
+        assertEquals(expected, PrintEvenElements.printEvenInts(numbers));
+    }
 }
