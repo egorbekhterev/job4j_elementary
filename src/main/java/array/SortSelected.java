d58464486df7e3c3854422c96fe86cd408bc4169
Paranoj <e.behterev@yandex.ru>
2022-05-30 17:21:49 +0700

    6.5.3. Сортировка выборкой. [#156320]

---
+++ src/main/java/array/SortSelected.java
@@ -0,0 +1,12 @@
+package array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            int min = MinDiaposon.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, i, index);
+        }
+        return data;
+    }
+}
